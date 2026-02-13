dd22c55c8a16a8cc69a02e658f2e71c19ce8c1b0
Serguyo <anthonycipriano68@gmail.com>
2026-02-13 15:19:19 -0800

    Add counting functionality with a while loop

---
+++ counting.py
@@ -0,0 +1,12 @@
+def main():
+  print("Starting While Loop - Print Count Variable")
+    # Enter Your Code Here:
+i = 0
+while i < 11:
+  print(i)
+  i += 1
+
+    
+print("Ending While Loop")
+
+main()
